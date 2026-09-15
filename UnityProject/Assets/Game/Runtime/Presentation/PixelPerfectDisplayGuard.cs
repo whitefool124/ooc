1deba6d2fc819c070203bd5d50c80f2d702cd240
whitefool124 <166610588+whitefool124@users.noreply.github.com>
2026-09-16 00:02:33 +0800

    feat: integrate Unity pixel perfect camera pipeline

--- UnityProject/Assets/Game/Runtime/Presentation/PixelPerfectDisplayGuard.cs
+++ UnityProject/Assets/Game/Runtime/Presentation/PixelPerfectDisplayGuard.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Reflection;
 
 namespace OCC.Combat.Presentation
 {
@@ -29,7 +31,36 @@ namespace OCC.Combat.Presentation
                 if (camera == null) continue;
                 camera.allowMSAA = false;
                 camera.allowHDR = false;
+                ConfigurePixelPerfectCamera(camera);
             }
+        }
+
+        private static void ConfigurePixelPerfectCamera(Camera camera)
+        {
+            Type type = FindPixelPerfectCameraType();
+            if (type == null) return;
+            Component component = camera.GetComponent(type) ?? camera.gameObject.AddComponent(type);
+            Set(type, component, "assetsPPU", 32);
+            Set(type, component, "refResolutionX", 480);
+            Set(type, component, "refResolutionY", 270);
+            Set(type, component, "upscaleRenderTexture", true);
+            Set(type, component, "pixelSnapping", true);
+        }
+
+        private static Type FindPixelPerfectCameraType()
+        {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                Type type = assembly.GetType("UnityEngine.U2D.PixelPerfectCamera", false);
+                if (type != null && typeof(Component).IsAssignableFrom(type)) return type;
+            }
+            return null;
+        }
+
+        private static void Set(Type type, Component component, string name, object value)
+        {
+            PropertyInfo property = type.GetProperty(name, BindingFlags.Instance | BindingFlags.Public);
+            if (property != null && property.CanWrite) property.SetValue(component, value, null);
         }
     }
 }
